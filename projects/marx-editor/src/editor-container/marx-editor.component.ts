8352ee526050a259ecbbc89690f852f94656fc10
Alec Aldrine Lakra <lakraalec123@gmail.com>
2021-03-03 15:17:27 +0530

    cleaning paste

--- projects/marx-editor/src/editor-container/marx-editor.component.ts
+++ projects/marx-editor/src/editor-container/marx-editor.component.ts
@@ -473,6 +473,13 @@ export class MarxEditorComponent implements OnInit, OnChanges, AfterViewInit, On
         const str = ' target="_blank" rel="noopener noreferrer"';
         return match + str;
       });
+
+      const bT = ['nav', 'script', 'applet', 'embed', 'noframes', 'noscript', 'form', 'meta', 'iframe'];
+
+      for (let i = 0; i < bT.length; i++) {
+        let tS = new RegExp('<' + bT[i] + '\\b.*>.*</' + bT[i] + '>', 'gi');
+        pastedHtml = pastedHtml.replace(tS, '');
+      }
      //  pastedHtml = this.cleanPaste(pastedHtml);
       document.execCommand('insertHtml', false, pastedHtml);
     }
